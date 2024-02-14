d4a9ff57281b0fe2c6c11ef57da5d52b46ee6fb4
김용선 <metalgos2@gmail.com>
2024-02-15 03:30:46 +0900

    vo객체를 이용한 파라미터 대입 밑 json 데이터 활용 실습

--- blog/src/main/java/com/cos/blog/test/HttpControllerTest.java
+++ blog/src/main/java/com/cos/blog/test/HttpControllerTest.java
@@ -7,19 +7,19 @@ import org.springframework.web.bind.annotation.*;
 public class HttpControllerTest {
 
     //http://localhost:8080/http/get
-    @GetMapping("/http/get")
-    public String getTest(){
-        return "get 요청";
+    @GetMapping("/http/get") //기본 브라우저 요청은 겟방식
+    public String getTest(Member m  ){
+        return "get 요청 " + m.getId() +" " +  m.getUsername()+" " +m.getPassword() +" " +m.getEmail();
     }
 
-    @PostMapping("/http/post")
-    public String posetTest(){
-        return "post 요청";
+    @PostMapping("/http/post") // 텍스트 플레인, 제이슨 테스트
+    public String posetTest(@RequestBody Member m){ //메시지 컨버터가 제이슨으로 변경
+        return "post 요청 "+ m.getId() +" " +  m.getUsername()+" " +m.getPassword() +" " +m.getEmail();
     }
 
     @PutMapping("/http/put")
-    public String putTest(){
-        return "put 요청";
+    public String putTest(@RequestBody Member m){
+        return "put 요청"+ m.getId() +" " +  m.getUsername()+" " +m.getPassword() +" " +m.getEmail();
     }
 
     @DeleteMapping("http/delete")
