cb07f0a1be946f772b4b915827ad9405bd45f41c
김용선 <metalgos2@gmail.com>
2024-02-26 00:45:28 +0900

    리플 테이블

--- blog/src/main/java/com/cos/blog/model/Reply.java
+++ blog/src/main/java/com/cos/blog/model/Reply.java
@@ -4,8 +4,11 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
 
 import javax.persistence.*;
+import java.nio.channels.Pipe;
+import java.sql.Timestamp;
 
 @Data
 @AllArgsConstructor
@@ -20,7 +23,16 @@ public class Reply {
    @Column(nullable = false , length = 200)
     private String content; // 섬머노트 라이브러리 html 태그가 섞여서 디자인이 됨
 
+    @ManyToOne //하나의 글에 여러개의 답글 가능
     @JoinColumn(name = "boardid")
     private Board board;
 
+    @ManyToOne
+    @JoinColumn(name = "userid")
+    private User user;
+
+    @CreationTimestamp
+    private Timestamp createDate;
+
+    
 }
