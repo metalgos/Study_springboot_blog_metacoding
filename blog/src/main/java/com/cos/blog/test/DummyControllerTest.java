dcd5941e865673e222dfa0ca49e77e9744edc519
김용선 <metalgos2@gmail.com>
2024-03-03 02:54:26 +0900

    페이징 page 사용 예

--- blog/src/main/java/com/cos/blog/test/DummyControllerTest.java
+++ blog/src/main/java/com/cos/blog/test/DummyControllerTest.java
@@ -4,11 +4,16 @@ import com.cos.blog.model.RoleType;
 import com.cos.blog.model.User;
 import com.cos.blog.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.function.Supplier;
 
@@ -19,10 +24,30 @@ public class DummyControllerTest {
     private UserRepository userRepository;
 
 
+    @GetMapping("/dummy/users")
+    public List<User> list() {
+
+        return userRepository.findAll();
+    }
+
+
+    @GetMapping("dummy/user")
+    public List<User> pageList(@PageableDefault(size = 2, sort = "id", direction = Sort.Direction.DESC) Pageable pageable) {
+
+        Page<User> pagingUser = userRepository.findAll(pageable);
+
+        //페이징이 첫번쨰 인가?
+        if(pagingUser.isFirst()){
+
+
+        }
+        List<User> users = pagingUser.getContent();
+        return users;
+    }
 
     //{id}로 주소 파라미터 전달받을수있음
     @GetMapping("/dummy/user/{id}")
-    public User detail(@PathVariable int id){
+    public User detail(@PathVariable int id) {
 
         //user/4를 요청시 데이터베이스에 내용이 없으면 값이 null
         //null값을 방지하기 위해 옵셔널로 값을 감싼다.
@@ -42,10 +67,10 @@ public class DummyControllerTest {
         });
 */
 
-        User user = userRepository.findById(id).orElseget(new Supplier<IllegalArgumentException>() {
+        User user = userRepository.findById(id).orElseThrow(new Supplier<IllegalArgumentException>() {
             @Override
             public IllegalArgumentException get() {
-                return new IllegalArgumentException("해당유저는 없습니다. id : "+id);
+                return new IllegalArgumentException("해당유저는 없습니다. id : " + id);
             }
         });
         //요청 : 웹
@@ -57,31 +82,22 @@ public class DummyControllerTest {
     }
 
 
-
-
-
-
-
-
-
-
-
     //http://localhost:8000/blog/dummy/join (요청)
     // http body에 데이터를 가지고 (요청)
     @PostMapping("/dummy/join")
 
     //public String join(String username, String password, String email){
 
-        //System.out.println("username : "+username);
-       // System.out.println("password : " +password);
-       // System.out.println("email : "+email);
+    //System.out.println("username : "+username);
+    // System.out.println("password : " +password);
+    // System.out.println("email : "+email);
 
-    public String join(User user){
+    public String join(User user) {
         System.out.println("id" + user.getId());
-        System.out.println("username : "+user.getUsername());
+        System.out.println("username : " + user.getUsername());
         System.out.println("password : " + user.getPassword());
-        System.out.println("email : "+user.getEmail());
-        System.out.println("role : "+user.getRole());
+        System.out.println("email : " + user.getEmail());
+        System.out.println("role : " + user.getRole());
 
         user.setRole(RoleType.USER); //enum으로 set
         userRepository.save(user);
