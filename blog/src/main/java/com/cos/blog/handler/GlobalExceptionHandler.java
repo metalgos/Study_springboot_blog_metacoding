2336ad28be55e1649ab11278dd213d0dc9bdd200
김용선 <metalgos2@gmail.com>
2024-03-07 11:43:52 +0900

    에러 exception 처리하기

--- blog/src/main/java/com/cos/blog/handler/GlobalExceptionHandler.java
+++ blog/src/main/java/com/cos/blog/handler/GlobalExceptionHandler.java
@@ -1,4 +1,21 @@
 package com.cos.blog.handler;
 
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+
+
+//에러 exception 처리하기
+@ControllerAdvice
+@RestController
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(value = Exception.class)
+    public String handleArgumentException(Exception e){
+
+        return "<h1>" +e.getMessage()+"</h1>";
+    }
+
+
 }
