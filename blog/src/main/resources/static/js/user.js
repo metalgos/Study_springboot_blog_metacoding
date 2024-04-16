fc97578b936ae2d90f78c146d21c726630008793
김용선 <metalgos2@gmail.com>
2024-04-17 00:55:36 +0900

    업데이트

--- blog/src/main/resources/static/js/user.js
+++ blog/src/main/resources/static/js/user.js
@@ -12,7 +12,6 @@ let index = {
             password: $("#password").val(), // 수정된 부분
             email: $("#email").val() // 수정된 부분
         }
-
        // console.log(data);
        $.ajax().dong().fail(): // ajax 톻신 이용해서 3개의 데이터를 json으로 변경하여 insert 요청!!
     }
